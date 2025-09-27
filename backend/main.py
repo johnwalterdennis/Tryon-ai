a9889ac9982da41b9c9330d45c42f6ebdf8dfaae
jaiden-lee <jaidenwlee@gmail.com>
2025-09-27 09:24:50 -0400

    selfie upload

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,8 @@ import os
 app = FastAPI()
 
 USER_UPLOAD_DIR = "static/user_uploads"
+SELFIE_UPLOAD_DIR = "static/selfie_uploads"
+current_user_selfie_path = None
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,4 +33,23 @@ async def upload_image(file: UploadFile = File(...)):
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     
-    return {"filename": temp_filename, "url": f"/files/{temp_filename}"}
+    return {"filename": temp_filename, "url": file_path}
+
+@app.post("/upload-selfie/")
+async def upload_selfie(file: UploadFile = File(...)):
+    # delete old selfies first
+    for existing_file in os.listdir(SELFIE_UPLOAD_DIR):
+        os.remove(os.path.join(SELFIE_UPLOAD_DIR, existing_file))
+        
+        
+    # new selfie
+    file_ext = os.path.splitext(file.filename)[1]
+    temp_filename = f"{uuid.uuid4().hex}{file_ext}"
+    file_path = os.path.join(SELFIE_UPLOAD_DIR, temp_filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+    current_user_selfie_path = file_path
+    
+    return {"filename": temp_filename, "url": file_path}
