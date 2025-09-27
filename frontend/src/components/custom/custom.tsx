16d85baa4b72245702044ab76ed536b6ddaf885c
jaiden-lee <jaidenwlee@gmail.com>
2025-09-27 19:53:41 -0400

    back button

--- frontend/src/components/custom/custom.tsx
+++ frontend/src/components/custom/custom.tsx
@@ -1,19 +1,12 @@
 export default function Custom() {
     return(
         <div
-            className="border-2 border-dashed border-darkpink/30 rounded-lg py-20 text-center bg-darkpink/10 cursor-pointer  mx-auto my-10 w-full"
+            className="border-2 border-dashed border-darkpink/30 rounded-lg h-96 text-center bg-darkpink/10 cursor-pointer  mx-auto my-10 w-full"
         >
-            <label htmlFor="upload-image" className="cursor-pointer block">
-                <div className="text-lg text-gray-500 mb-4">
+            <label className="cursor-pointer h-full w-full flex items-center justify-center">
+                <div className="text-lg text-darkpink/50 font-semibold mb-4">
                     Drag & drop images here, or click to upload
                 </div>
-                <input
-                    id="upload-image"
-                    type="file"
-                    accept="image/*"
-                    multiple
-                    className="hidden"
-                />
             </label>
         </div>
     )
