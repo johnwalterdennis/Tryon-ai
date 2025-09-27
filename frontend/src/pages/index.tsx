72d2b5ace0c9532c3755d1f6ded68d5518399d21
mvu46 <allisonvu9@gmail.com>
2025-09-26 22:32:58 -0400

    landing page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <p>hi</p>
+    <div className="flex flex-col items-center">
+      <p>Sed ut perspiciatis unde omnis iste natus error</p>
+      <Link href="/tryon" className="rounded-xl bg-blue-400 px-12 py-4 hover:bg-blue-700 hover:scale-105 transition-all text-white text-2xl font-bold">
+      Try Now
+      </Link>
     </div>
   );
 }
