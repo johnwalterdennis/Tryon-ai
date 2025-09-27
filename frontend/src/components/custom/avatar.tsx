4555d950988472c287cdf59cf01dbcd2e6e382cd
mvu46 <allisonvu9@gmail.com>
2025-09-27 19:21:28 -0400

    backend stuff

--- frontend/src/components/custom/avatar.tsx
+++ frontend/src/components/custom/avatar.tsx
@@ -5,29 +5,36 @@ type Property = {
   selectedOutfitID: string | undefined;
 };
 
-export default function Avatar({selectedOutfitID} : Property) {
-    const [avatar, setAvatar] = useState<string | undefined>(undefined);
+export default function Avatar({ selectedOutfitID }: Property) {
+  const [avatar, setAvatar] = useState<string | undefined>(undefined);
 
-    useEffect(()=>{
-        async function fetchAvatar() {
-            try {
-                const result = await axios.get("http://localhost:8000/get-generated-outfit", {
-                    params: { outfit: selectedOutfitID}
-                });
-                console.log(result)
-            } catch {
-                console.log("fail")
-            }
+  useEffect(() => {
+    async function fetchAvatar() {
+      try {
+        const result = await axios.get(
+          "http://localhost:8000/get-generated-outfit",
+          {
+            params: { outfit: selectedOutfitID },
+          }
+        );
+        if (result.data && result.data.url) {
+          setAvatar(result.data.url);
         }
+      } catch {
+        console.log("fail");
+      }
+    }
 
-        fetchAvatar();
-    }, [selectedOutfitID]);
+    fetchAvatar();
+  }, [selectedOutfitID]);
 
-    return(
-        <div className="h-full">
-            {
-                !avatar ? <div className="flex w-full h-full justify-center items-center" ><div className="bg-darkpink/20 w-[32rem] h-[76.5%] aspect-square rounded-2xl"></div></div> : <img src="" alt="hi"/>
-            }
+  return (
+    <div className="h-full">
+      <div className="flex w-full h-full justify-center items-center">
+        <div className="bg-darkpink/20 w-[32rem] h-[76.5%] aspect-square rounded-2xl">
+          {avatar && <img src={avatar} alt="hi" className="object-cover h-full w-full" />}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
