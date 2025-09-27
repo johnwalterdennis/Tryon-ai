ebbc53b2d87adc0af45f69e1e2030e75574f7523
mvu46 <allisonvu9@gmail.com>
2025-09-27 00:46:00 -0400

    fake carousel

---
+++ frontend/src/components/custom/fit-carousel.tsx
@@ -0,0 +1,18 @@
+const outfitImages = [
+  "/landing-page/1.png",
+  "/landing-page/2.jpeg",
+  "/landing-page/3.jpeg",
+  "/landing-page/4.jpeg",
+  "/landing-page/5.jpeg",
+];
+export default function FitCarousel() {
+  return (
+    <div className="absolute -z-10 flex gap-6 justify-center items-center overflow-hidden">
+      <img src={outfitImages[1]} className="w-[15%] rounded-xl" />
+      <img src={outfitImages[2]} className="w-[20%] rounded-xl" />
+      <img src={outfitImages[0]} className="w-[25%] rounded-xl" />
+      <img src={outfitImages[3]} className="w-[20%] rounded-xl" />
+      <img src={outfitImages[4]} className="w-[15%] rounded-xl" />
+    </div>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,30 @@
 import Link from "next/link";
+import { Typewriter } from "react-simple-typewriter";
+import FitCarousel from "@/components/custom/fit-carousel";
+
 
 export default function Home() {
+  
   return (
-    <div className="flex flex-col items-center">
-      <p>Sed ut perspiciatis unde omnis iste natus error</p>
-      <Link href="/tryon" className="rounded-xl bg-blue-400 px-12 py-4 hover:bg-blue-700 hover:scale-105 transition-all text-white text-2xl font-bold">
-      Try Now
+    <div className="flex flex-col items-center h-screen justify-center gap-16">
+      <FitCarousel/>
+      <p className="text-6xl font-bold z-10">
+        See yourself in
+        <Typewriter
+          words={[" anything!", " pants!", " shirts!", " hats!"]}
+          loop={0}
+          cursor
+          cursorStyle="|"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1500}
+        />
+      </p>
+      <Link
+        href="/tryon"
+        className="rounded-xl bg-green px-12 py-4 hover:scale-105 transition-all text-white text-2xl font-bold"
+      >
+        Try Now
       </Link>
     </div>
   );
