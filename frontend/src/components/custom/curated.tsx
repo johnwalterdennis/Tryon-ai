bcefe1c05f84c670cc70efe278c9d085cc02baa1
mvu46 <allisonvu9@gmail.com>
2025-09-27 15:17:07 -0400

    Checkout button

--- frontend/src/components/custom/curated.tsx
+++ frontend/src/components/custom/curated.tsx
@@ -5,6 +5,9 @@ export default function Curated() {
   return (
     <div className="h-[90%]">
       <CardContainer />
+      <div className="flex justify-center items-center mt-4">
+        <button className="bg-green w-full p-4 text-white rounded-2xl font-bold hover:scale-105 transition-all">Checkout</button>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/tryon.tsx
+++ frontend/src/pages/tryon.tsx
@@ -4,7 +4,7 @@ import Custom from "@/components/custom/custom";
 import ViewToggle from "@/components/custom/viewtoggle";
 import Avatar from "@/components/custom/avatar";
 
-type View = 'curated' | 'custom';
+type View = "curated" | "custom";
 
 export default function TryOn() {
   const [view, setView] = useState<View>("curated");
@@ -13,10 +13,10 @@ export default function TryOn() {
     <div className="flex p-6 h-screen">
       <div className="w-[45%] h-full">
         <ViewToggle activeView={view} setView={setView} />
-        {view === 'curated' ? <Curated /> : <Custom />}
+        {view === "curated" ? <Curated /> : <Custom />}
       </div>
       <div className="w-[55%]">
-        <Avatar/>
+        <Avatar />
       </div>
     </div>
   );
