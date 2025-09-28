31062b1cd2fe4a8d391c0a3cb6f95545be3e6c66
mvu46 <allisonvu9@gmail.com>
2025-09-27 21:04:00 -0400

    homepage fix

--- frontend/src/components/custom/navbar.tsx
+++ frontend/src/components/custom/navbar.tsx
@@ -8,16 +8,26 @@ const items = [
 ]
 export default function Navbar() {
     return(
-        <div className="absolute top-0 flex justify-between w-full px-12 py-4 border-b-[2px] border-b-darkpink/10">
-            <div className="flex gap-10 font-medium items-center">
+        <div className="absolute top-0 flex justify-between w-full px-16 py-3 border-b-[2px] border-b-darkpink/10">
+            <div className="flex gap-10 items-center text-sm">
                 <Image className="w-10" src={"/landing-page/logo.svg"} alt={"logo"} width={800} height={800}/>
-                {items.map((item, i) => {
-                    return (<p key={i}>{item}</p>)
-                })}
+                {items.map((item, i) => (
+                    <span key={i} className="flex items-center gap-1 cursor-pointer">
+                        {item}
+                        {item !== "Pricing" && (
+                            <Image
+                                src="/landing-page/dropdown.svg"
+                                alt="Dropdown indicator"
+                                width={20}
+                                height={6}
+                            />
+                        )}
+                    </span>
+                ))}
             </div>
-            <div className="flex items-center gap-10 font-medium">
-                <p>Log in</p>
-                <button className="bg-black py-3 px-4 text-white rounded-lg font-semibold">Get started for free</button>
+            <div className="flex items-center gap-4 text-sm font-medium">
+                <p className="border-1 rounded-lg py-3 px-4 cursor-pointer hover:scale-[1.02] transition-all">Log in</p>
+                <button className="bg-black py-3 px-4 text-white rounded-lg font-semibold cursor-pointer hover:scale-[1.02] transition-all">Get started for free</button>
             </div>
         </div>
     )
