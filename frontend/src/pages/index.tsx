d48a1c7cc69ec14600676ad5e3930bb74b0cf3d9
mvu46 <allisonvu9@gmail.com>
2025-09-27 20:36:03 -0400

    landing page change

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <div className="flex flex-col items-center h-screen justify-center gap-16">
       <Navbar/>
       <FitCarousel />
-      <div className="bg-white/45 backdrop-blur-sm flex flex-col items-center gap-16 w-[60%] py-16 rounded-2xl">
-        <p className="text-6xl font-bold z-10">
+      <div className="bg-darkpink/50 backdrop-blur-sm flex flex-col items-center gap-16 w-[60%] py-16 rounded-2xl shadow-black/30 shadow-xl">
+        <p className="text-6xl font-bold z-10 text-rose-950 cursor-default">
           See yourself in
           <Typewriter
             words={[" anything!", " pants!", " shirts!", " hats!"]}
@@ -23,7 +23,7 @@ export default function Home() {
         </p>
         <Link
           href="/uploadpage"
-          className="rounded-xl bg-darkpink px-12 py-4 hover:scale-105 transition-all text-white text-2xl font-bold"
+          className="rounded-xl bg-darkpink/90 px-12 py-4 hover:scale-105 transition-all text-rose-950 text-2xl font-bold  shadow-black/30 shadow-xl"
         >
           Try now
         </Link>
